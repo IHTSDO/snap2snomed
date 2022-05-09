69a785745de81a832b0048752734dbe01148c1eb
dionmcm <dion.mcmurtrie@gmail.com>
2022-05-10 09:25:37 +1000

    fixed field length in UI for note_text to match DB

--- ui/snapclient/src/app/_utils/form_utils.ts
+++ ui/snapclient/src/app/_utils/form_utils.ts
@@ -19,7 +19,7 @@ export class FormUtils {
   static readonly MAX_VERSION = 30;
   static readonly MAX_DESCRIPTION = 200;
   static readonly MAX_TARGETSCOPE = 1024;
-  static readonly MAX_NOTE = 256;
+  static readonly MAX_NOTE = 255;
   static readonly MAX_TASK_DESCRIPTION = 60;
   static readonly VALID_STRING_PATTERN = /^\S+.*/;
 };
