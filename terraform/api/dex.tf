1cec323c753d9b73d13584832bec62e559e80346
Attila Edelenyi <ede_ati@hotmail.com>
2022-05-19 15:48:23 +1000

    Wrong log group name

--- terraform/api/dex.tf
+++ terraform/api/dex.tf
@@ -22,7 +22,7 @@ resource "aws_ecs_task_definition" "dex" {
       logConfiguration = {
         logDriver = "awslogs"
         options = {
-          awslogs-group         = aws_cloudwatch_log_group.api.name
+          awslogs-group         = aws_cloudwatch_log_group.dex.name
           awslogs-region        = var.aws_region
           awslogs-stream-prefix = "snap2snomed-dex"
         }
