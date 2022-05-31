6edfd35da129eefc6abb025706dedf942b703fa9
Attila Edelenyi <ede_ati@hotmail.com>
2022-05-31 12:51:05 +1000

    Add karma spec reporter to find out more on invalid token issue

--- ui/snapclient/karma.conf.js
+++ ui/snapclient/karma.conf.js
@@ -25,6 +25,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-spec-reporter'),
       require('karma-junit-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -49,7 +50,7 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml', 'junit'],
+    reporters: ['progress', 'kjhtml', 'junit', 'spec'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
