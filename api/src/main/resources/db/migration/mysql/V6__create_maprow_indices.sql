f92c68f9e0ed91cd832266e30513384124bc91d2
Dilys Li <li306@csiro.au>
2022-06-17 10:47:58 +1000

    fix sql

---
+++ api/src/main/resources/db/migration/mysql/V6__create_maprow_indices.sql
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2022 SNOMED International
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ALTER TABLE map_row ADD INDEX author_task_idx (author_task_id);
+ALTER TABLE map_row ADD INDEX review_task_idx (review_task_id);
