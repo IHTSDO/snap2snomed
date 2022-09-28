586b99463beaaf3740d564db1345faa893d75338
Deanne Ukovich <Deanne.Ukovich@csiro.au>
2022-09-28 14:40:27 +1000

    develop is broken, suspect this might be the cause but works locally

--- ui/snapclient/src/app/app-routing.module.ts
+++ ui/snapclient/src/app/app-routing.module.ts
@@ -100,7 +100,7 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes, {
     anchorScrolling: 'enabled',
     scrollPositionRestoration: 'enabled',
-    initialNavigation: 'enabledNonBlocking', 
+    initialNavigation: 'enabled', 
     onSameUrlNavigation: 'reload'
   })],
   exports: [RouterModule]
