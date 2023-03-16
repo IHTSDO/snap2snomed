5db8de97042bce15e6dbec1538034026eba8d1e6
Deanne Ukovich <Deanne.Ukovich@csiro.au>
2023-03-16 13:00:12 +1000

    commons-csv returning different error code

--- api/src/test/java/org/snomed/snap2snomed/integration/controller/ImportedMappingRestControllerIT.java
+++ api/src/test/java/org/snomed/snap2snomed/integration/controller/ImportedMappingRestControllerIT.java
@@ -218,7 +218,7 @@ public class ImportedMappingRestControllerIT extends IntegrationTestBase {
         400, null, DEFAULT_TEST_USER_SUBJECT);
     restClient.expectCreateImportedMapFail(0, 2, 3, 4, -1, -1, true, ",",
         new ClassPathResource("AAA_invalid_csv_doublequotes.csv").getFile(), "text/csv", mapId,
-        400, null, DEFAULT_TEST_USER_SUBJECT);
+        500, null, DEFAULT_TEST_USER_SUBJECT);
   }
 
   /**
@@ -229,7 +229,7 @@ public class ImportedMappingRestControllerIT extends IntegrationTestBase {
     restClient.expectCreateImportedCodeSetFail("badAAA", "2", 0, 2, true, "\t",
         new ClassPathResource("AAA_invalid_csv_mixeddelimiters.csv").getFile(), "text/tsv", 400, null);
     restClient.expectCreateImportedCodeSetFail("badAAA", "2", 0, 2, true, ",",
-        new ClassPathResource("AAA_invalid_csv_doublequotes.tsv").getFile(), "text/tsv", 400, null);
+        new ClassPathResource("AAA_invalid_csv_doublequotes.tsv").getFile(), "text/tsv", 500, null);
   }
 
   /**
