cc366035fad984b814faf01700329ce1a899515c
Deanne Ukovich <Deanne.Ukovich@csiro.au>
2023-05-31 12:07:45 +1000

    remove not null restrictions on the audit tables for the new columns

--- api/src/main/resources/db/migration/mysql/V13__dual_mapping_mode.sql
+++ api/src/main/resources/db/migration/mysql/V13__dual_mapping_mode.sql
@@ -15,14 +15,14 @@
  */
 
 ALTER TABLE project ADD dual_map_mode BOOLEAN NOT NULL DEFAULT FALSE;
-ALTER TABLE project_aud ADD dual_map_mode BOOLEAN NOT NULL DEFAULT FALSE;
+ALTER TABLE project_aud ADD dual_map_mode BOOLEAN;
 
 ALTER TABLE map_row DROP FOREIGN KEY FK93uu5g46v77a1uah6ck1enwwl;
 ALTER TABLE map_row DROP CONSTRAINT UniqueMapAndSourceCode;
 ALTER TABLE map_row ADD CONSTRAINT FK93uu5g46v77a1uah6ck1enwwl FOREIGN KEY (map_id) REFERENCES map (id);
 
 ALTER TABLE map_row ADD blind_map_flag BOOLEAN NOT NULL DEFAULT FALSE;
-ALTER TABLE map_row_aud ADD blind_map_flag BOOLEAN NOT NULL DEFAULT FALSE;
+ALTER TABLE map_row_aud ADD blind_map_flag BOOLEAN;
 
 ALTER TABLE map_row ADD reconcile_task_id BIGINT;
 ALTER TABLE map_row_aud ADD reconcile_task_id BIGINT;
@@ -41,11 +41,11 @@ ALTER TABLE map_row ADD INDEX reconcile_task_idx (reconcile_task_id);
    QueryDSL and then "selectNew" cannot be called within a union https://github.com/Blazebit/blaze-persistence/issues/565" 
 */
 CREATE OR REPLACE VIEW map_view AS 
-    SELECT id AS map_row_id, status, blind_map_flag, null as sibling_row_author_task_id
+    SELECT UUID() as 'id', id AS map_row_id, status, blind_map_flag, null as sibling_row_author_task_id
     FROM map_row 
     WHERE map_row.blind_map_flag = false
 UNION
-    SELECT mr1.id AS map_row_id, (CASE WHEN mr1.status != mr2.status THEN '1' ELSE mr1.status END), mr1.blind_map_flag,
+    SELECT UUID() as 'id', mr1.id AS map_row_id, (CASE WHEN mr1.status != mr2.status THEN '1' ELSE mr1.status END), mr1.blind_map_flag,
     mr2.author_task_id
     FROM map_row mr1, map_row mr2
     WHERE mr1.source_code_id = mr2.source_code_id
