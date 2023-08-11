fbd8a670bc84c64174f4b0905f02f5dac11d3b94
Michael Lawley <michael.lawley@csiro.au>
2023-08-11 14:21:17 +1000

    add missing migration

---
+++ api/src/main/resources/db/migration/mysql/V13__add_imported_codeset_metadata.sql
@@ -0,0 +1,5 @@
+alter table imported_codeset add system_uri VARCHAR(255);
+alter table imported_codeset add valueset_uri VARCHAR(255);
+
+alter table imported_codeset_aud add system_uri VARCHAR(255);
+alter table imported_codeset_aud add valueset_uri VARCHAR(255);
