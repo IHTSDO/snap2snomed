e61fc8209cf7bed070722645bacdff8f96dd0125
Edelenyi <attila.edelenyi@csiro.au>
2025-09-11 14:08:03 +1000

    Remove DEX Provider as it doesn't exist anymore

--- terraform/cognito/oidcprovider-dex.tf
+++ terraform/cognito/oidcprovider-dex.tf
@@ -1,19 +1,20 @@
-resource "aws_cognito_identity_provider" "dex" {
-  user_pool_id  = aws_cognito_user_pool.userpool.id
-  provider_name = var.expected_snomed_idp_name
-  provider_type = "OIDC"
-  provider_details = {
-    attributes_request_method = "GET"
-    oidc_issuer               = format("%sidp/dex", var.api_url)
-    authorize_scopes          = "profile openid email"
-    client_id                 = "snap2snomed"
-    client_secret             = var.dex_client_secret
-  }
-  attribute_mapping = {
-    email          = "email"
-    email_verified = "email_verified"
-    given_name     = "given_name"
-    family_name    = "family_name"
-    username       = "sub"
-  }
-}
+# DEX Provider is removed
+# resource "aws_cognito_identity_provider" "dex" {
+#   user_pool_id  = aws_cognito_user_pool.userpool.id
+#   provider_name = var.expected_snomed_idp_name
+#   provider_type = "OIDC"
+#   provider_details = {
+#     attributes_request_method = "GET"
+#     oidc_issuer               = format("%sidp/dex", var.api_url)
+#     authorize_scopes          = "profile openid email"
+#     client_id                 = "snap2snomed"
+#     client_secret             = var.dex_client_secret
+#   }
+#   attribute_mapping = {
+#     email          = "email"
+#     email_verified = "email_verified"
+#     given_name     = "given_name"
+#     family_name    = "family_name"
+#     username       = "sub"
+#   }
+# }
