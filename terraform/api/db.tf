36dab5e1ae8ba510c6c5930ddce945ecd7450276
Edelenyi <attila.edelenyi@csiro.au>
2026-03-12 13:49:12 +1000

    Rotate the DB password every month

--- terraform/api/db.tf
+++ terraform/api/db.tf
@@ -16,6 +16,15 @@ resource "aws_rds_cluster" "api" {
   }
 }
 
+resource "aws_secretsmanager_secret_rotation" "db" {
+  secret_id          = aws_rds_cluster.api.master_user_secret[0].secret_arn
+  rotate_immediately = false
+
+  rotation_rules {
+    automatically_after_days = 30
+  }
+}
+
 resource "aws_rds_cluster_instance" "api" {
   cluster_identifier      = aws_rds_cluster.api.id
   engine                  = "aurora-mysql"
